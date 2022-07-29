ca75697916e5a83b58c74215d790b3e907e05ee7
Anubha13kumari <anubhamuz@gmail.com>
2022-07-29 17:40:52 +0530

    header and card section done

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,29 @@
 import React from "react";
-import { GrSearch } from "react-icons/gr";
-import './Header.css'
+import "./Header.css";
+import { Button } from "@mui/material";
 
 function Header() {
   return (
     <div className="header__container">
-        <div className="header__title">
-      <h1>Search Properties for Rent</h1>
-      <div className="header__search">
-        <input className="header__searchInput" type="text">
-         </input>
-        <GrSearch className="header__searchIcon" />
+      <div className="header__title">
+        <h1>Search Properties for Rent</h1>
+        <div className="header__search"></div>
       </div>
-     </div>
-     <div>
-        filters
-        </div>
-      
+      <div className="header__filtercontainer">
+        <Button
+          style={{
+            backgroundColor: "#8c52ff",
+            color: "white",
+            width: "8vw",
+            height: "7vh",
+            marginRight: "1rem",
+            marginTop: "0.75rem",
+          }}
+          variant="contained"
+        >
+          Search
+        </Button>
+      </div>
     </div>
   );
 }
