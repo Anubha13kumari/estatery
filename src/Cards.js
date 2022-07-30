b6b23aab24f327cc85703c296b1875a9b8dc8c4b
Anubha13kumari <anubhamuz@gmail.com>
2022-07-30 17:11:14 +0530

    filter method is done

--- src/Cards.js
+++ src/Cards.js
@@ -1,260 +1,145 @@
-import React from "react";
-import './Cards.css'
-import {BsSuitHeart} from "react-icons/bs"
-import {MdOutlineBed} from "react-icons/md"
-import {BiBath} from "react-icons/bi"
-import {TbSquareRotated} from "react-icons/tb"
+import React, { useState } from "react";
+import "./Cards.css";
+import { BsSuitHeart } from "react-icons/bs";
+import { MdOutlineBed } from "react-icons/md";
+import { BiBath } from "react-icons/bi";
+import { TbSquareRotated } from "react-icons/tb";
 import Header from "./Header";
 
 function Cards() {
+  const data = [
+    {
+      imageUrl:
+        "https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      price: " 2,400",
+      title: "Palm Harbour ",
+      location:
+        "New York,USA",
+      noOfBathroom: "2 bathrooms",
+      noOfBeds: "3 beds",
+      area: "5*7 m2",
+      property: "Bunglow",
+    },
+    {
+      imageUrl:
+        "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2021/08/download-23.jpg",
+      price: 5000,
+      title: "Albert gue",
+      location: "Texas,USA",
+      noOfBathroom: "3 bathrooms",
+      noOfBeds: "4 beds",
+      area: "9*6 m2",
+      property: "Bunglow",
+    },
+    {
+      imageUrl:
+        "https://assets.architecturaldigest.in/photos/60083e76274aca243711c3a4/16:9/w_2560%2Cc_limit/ghaziabad-uttar-pradesh-homes-photos-1366x768.jpg",
+      price: 1800,
+      title: "Paul",
+      location: "New York,USA",
+      noOfBathroom: "1 bathroom",
+      noOfBeds: "2 beds",
+      area: "4*3 m2",
+      property: "Bunglow",
+    },
+    {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBFAcoYUKdmY1Zkr67auyU4KYNJfI9SRONDHkWmHVCOSDoKuyYfY74CkRiEDhRE_lUMdo&usqp=CAU",
+      price: 3000,
+      title: "Robert",
+      location: "California,USA",
+      noOfBathroom: "3 bathrooms",
+      noOfBeds: "5 beds",
+      area: "9*8 m2",
+      property: "House",
+    },
+    {
+      imageUrl:
+        "https://img.onmanorama.com/content/dam/mm/en/lifestyle/decor/images/2021/4/4/tiruvalla-nri-home-c.jpg",
+      price: 4500,
+      title: "Hibert",
+      location: "California,USA",
+      noOfBathroom: "4 bathrooms",
+      noOfBeds: "6 beds",
+      area: "12*15 m2",
+      property: "Flat",
+    },
+    {
+      imageUrl:
+        "https://2.bp.blogspot.com/-z_OXnm2lnR8/W7X_MZy96TI/AAAAAAABO9k/di_jCEarBOMiXAhexS8cKd1e7qpg2Vk6gCLcBGAs/s1920/flat-contemporary-residence.jpg",
+      price: 8000,
+      title: "Oliver",
+      location: "Texas,USA",
+      noOfBathroom: "6 bathrooms",
+      noOfBeds: "7 beds",
+      area: "25*18 m2",
+      property: "Bunglow",
+    },
+    {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtPPbztZJ5wnmscxpz6l1hAh7Z3O9xoDFPPQ&usqp=CAU",
+      price: 3500,
+      title: "William",
+      location: "Texas,USA",
+      noOfBathroom: "3 bathrooms",
+      noOfBeds: "5 beds",
+      area: "10*7 m2 ",
+      property: "Flat",
+    },
+    {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtPPbztZJ5wnmscxpz6l1hAh7Z3O9xoDFPPQ&usqp=CAU",
+      price: 3500,
+      title: "William",
+      location: "New York,USA",
+      noOfBathroom: "3 bathrooms",
+      noOfBeds: "5 beds",
+      area: "10*7 m2 ",
+      property: "House",
+    },
+    {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtPPbztZJ5wnmscxpz6l1hAh7Z3O9xoDFPPQ&usqp=CAU",
+      price: 3500,
+      title: "William",
+      location: "California,USA",
+      noOfBathroom: "3 bathrooms",
+      noOfBeds: "5 beds",
+      area: "10*7 m2 ",
+      property: "House",
+    },
+  ];
+  const [filterData, setFilterData] = useState(data);
+
   return (
     <div className="cards__body">
-      <Header/>
+      <Header setFilterData={setFilterData} filterData={filterData} data={data}/>
       <div className="cards__container">
-      
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-              alt=""
-            ></img>
-            <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
+        {filterData.map((item) => (
+          <article className="cards__card">
+            <div className="stathome__div">
+              <img className="stathome__img" src={item.imageUrl} alt=""></img>
+              <h5 className="stathome__info">${item.price}<small>/per month</small></h5>
+              
+              <div className="cards__heart">
+                <BsSuitHeart className="card__icon" />
+              </div>
             </div>
+            <div className="stathome__quantity">
+            <small>{item.property}</small>
+              <h3>{item.title}</h3>
+            <small>{item.location}</small>
             </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
-
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2021/08/download-23.jpg"
-              alt=""
-            ></img>
-            <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
+            
+            <div>
+              <div>
+                <MdOutlineBed className="card__icon" /> {item.noOfBeds} <BiBath className="card__icon" />
+                {item.noOfBathroom} <TbSquareRotated className="card__icon" />
+                {item.area}
+              </div>
             </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
-
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://assets.architecturaldigest.in/photos/60083e76274aca243711c3a4/16:9/w_2560%2Cc_limit/ghaziabad-uttar-pradesh-homes-photos-1366x768.jpg"
-              alt=""
-            ></img>
-             <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
-            </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
-
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBFAcoYUKdmY1Zkr67auyU4KYNJfI9SRONDHkWmHVCOSDoKuyYfY74CkRiEDhRE_lUMdo&usqp=CAU"
-              alt=""
-            ></img>
-             <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
-            </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://img.onmanorama.com/content/dam/mm/en/lifestyle/decor/images/2021/4/4/tiruvalla-nri-home-c.jpg"
-              alt=""
-            ></img>
-            <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
-            </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed className="cards__icon"/> 3 bed <BiBath  className="cards__icon"/>2 bathroom <TbSquareRotated  className="cards__icon"/> 5*7 m2</div> 
-          </div>
-        </article>
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://2.bp.blogspot.com/-z_OXnm2lnR8/W7X_MZy96TI/AAAAAAABO9k/di_jCEarBOMiXAhexS8cKd1e7qpg2Vk6gCLcBGAs/s1920/flat-contemporary-residence.jpg"
-              alt=""
-            ></img>
-             <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
-            </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://www.thespruce.com/thmb/oOkzy1q-WtCPiwUwCjH1JQQ_NEE=/1500x844/smart/filters:no_upscale()/a-frame-houses-4772019-hero-7cacd243cfe74fb8b06f44760ea59f35.jpg"
-              alt=""
-            ></img>
-             <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
-            </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtPPbztZJ5wnmscxpz6l1hAh7Z3O9xoDFPPQ&usqp=CAU"
-              alt=""
-            ></img>
-             <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
-            </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://static.dezeen.com/uploads/2020/06/royal-william-kaven-portland-oregon_dezeen_hero.jpg"
-              alt=""
-            ></img>
-            <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
-            </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://empire-s3-production.bobvila.com/slides/8507/original/putty_house.jpg?1600126240"
-              alt=""
-            ></img>
-            <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
-            </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/wp-content/uploads/2021/08/download-11.jpg"
-              alt=""
-            ></img>
-             <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
-            </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
-        <article className="cards__card">
-          <div className="stathome__div">
-            <img
-              className="stathome__img"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZqYO8b4ED-RSHxIJ9tSNNGe44vPC06ALcmQ&usqp=CAU"
-              alt=""
-            ></img>
-             <h5 className="stathome__info">$2,400<small>/per month</small></h5>
-            <div className="cards__heart">
-            <BsSuitHeart/>
-            </div>
-            </div>
-          <div className="stathome__quantity">
-            <h3>Palm Harbour </h3>
-            <small>c/o henery Roth,50 Oakland Ave,#206 A  city,Florida,32104 United States of America</small>
-          </div>
-          <div>
-           <div><MdOutlineBed/> 3 bed <BiBath/>2 bathroom <TbSquareRotated/> 5*7 m2</div> 
-          </div>
-        </article>
+          </article>
+        ))}
       </div>
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Header.css";
 import { Button } from "@mui/material";
 import Box from "@mui/material/Box";
@@ -7,10 +7,11 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 
-function Header() {
+function Header({ setFilterData, filterData ,data}) {
+  const [filter,setFilter]=React.useState(false);
   const [location, setLocation] = React.useState("");
 
-  const handleChangelocation = (event) => {
+  const handleChangeLocation = (event) => {
     setLocation(event.target.value);
   };
   const [when, setWhen] = React.useState("");
@@ -20,15 +21,31 @@ function Header() {
   };
   const [price, setPrice] = React.useState("");
 
-  const handleChangeprice = (event) => {
+  const handleChangePrice = (event) => {
     setPrice(event.target.value);
   };
   const [property, setProperty] = React.useState("");
 
-  const handleChangeproperty = (event) => {
+  const handleChangeProperty = (event) => {
     setProperty(event.target.value);
   };
-
+  useEffect(() => {
+    let tempData=[...data]
+   if(property ){
+   tempData=tempData.filter((item)=>item.property==property)
+}
+if(price ){
+  let priceArray=price.split("-");
+  let minimumPrice=parseInt(priceArray[0]);
+  let maximumPrice=parseInt(priceArray[1]);
+  tempData=tempData.filter((item)=>item.price >= minimumPrice && item.price <= maximumPrice)
+}
+if(location ){
+  tempData=tempData.filter((item)=>item.location==location)
+}
+  setFilterData(tempData);
+  setFilter(false);
+  }, [filter]);
   return (
     <div className="header__container">
       <div className="header__title">
@@ -47,20 +64,20 @@ function Header() {
           }}
         >
           <FormControl style={{ flex: "auto" }}>
-            <InputLabel  id="demo-simple-select-label">Location</InputLabel>
+            <InputLabel id="demo-simple-select-label">Location</InputLabel>
             <Select
               sx={{
                 border: "none",
-                }}
+              }}
               labelId="demo-simple-select-label"
               id="demo-simple-select"
               value={location}
               label="Location"
-              onChange={handleChangelocation}
+              onChange={handleChangeLocation}
             >
-              <MenuItem value={"New York USA"}>New york,USA</MenuItem>
-              <MenuItem value={"Texas USA"}>Texas,USA</MenuItem>
-              <MenuItem value={"California USA"}>California,USA</MenuItem>
+              <MenuItem value={"New York,USA"}>New york,USA</MenuItem>
+              <MenuItem value={"Texas,USA"}>Texas,USA</MenuItem>
+              <MenuItem value={"California,USA"}>California,USA</MenuItem>
             </Select>
           </FormControl>
 
@@ -85,11 +102,11 @@ function Header() {
               id="demo-simple-select"
               value={price}
               label="Price"
-              onChange={handleChangeprice}
+              onChange={handleChangePrice}
             >
-              <MenuItem value={"$500-$2000"}>$500-$2000</MenuItem>
-              <MenuItem value={"$2000-$5000"}>$2000-$5000</MenuItem>
-              <MenuItem value={"$5000-$8000"}>$5000-$8000</MenuItem>
+              <MenuItem value={"500-2000"}>$500-$2000</MenuItem>
+              <MenuItem value={"2000-5000"}>$2000-$5000</MenuItem>
+              <MenuItem value={"5000-8000"}>$5000-$8000</MenuItem>
             </Select>
           </FormControl>
           <FormControl style={{ flex: "auto" }}>
@@ -99,11 +116,11 @@ function Header() {
               id="demo-simple-select"
               value={property}
               label="Property Type"
-              onChange={handleChangeproperty}
+              onChange={handleChangeProperty}
             >
-              <MenuItem value={"Houses"}>Houses</MenuItem>
+              <MenuItem value={"House"}>House</MenuItem>
               <MenuItem value={"Bunglow"}>Bunglow</MenuItem>
-              <MenuItem value={"Flats"}>Flats</MenuItem>
+              <MenuItem value={"Flat"}>Flat</MenuItem>
             </Select>
           </FormControl>
           <Button
@@ -114,6 +131,7 @@ function Header() {
               height: "7vh",
             }}
             variant="contained"
+            onClick={()=> setFilter(true)}
           >
             Search
           </Button>
